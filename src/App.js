caa0efe2bee6416acec4c1a90e4341e5802a1bc2
Jeremy Greenwood <jgreenwd@hotmail.com>
2020-03-15 18:34:52 -0500

    style: style page contents <640 pixels

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
     return (
         <div className="App">
             <header className="header">
-                <img src={logo} alt="logo"/>
-                <nav className="nav">
-                    <button className="nav__toggle" onClick={ () => {
+                <img src={logo} className="logo" alt="logo"/>
+                <button className="nav__toggle" onClick={ () => {
                         document.body.classList.toggle('nav-open'); }}>
-                        <span className="menu"></span>
-                    </button>
+                        <span className="nav__button"></span>
+                </button>
+                <nav className="nav">
                     <ul className="nav__list">
                         <li className="nav__item">
                             <a className="nav__link" href="https://jgreenwd.github.io/musician-cards">Musician Cards Project</a>
@@ -37,7 +37,7 @@ function App() {
                 Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </aside>
 
-            <footer className="content">
+            <footer className="footer">
                 Copyright © 2020 J. Greenwood
             </footer>
         </div>
