0824893e186d566d9d27c739c494707869cb81ba
Jeremy Greenwood <jgreenwd@hotmail.com>
2020-03-16 11:12:36 -0500

    Updates

---
+++ precache-manifest.290664f98522a001de1a105f6e3ca42d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d59d335438d347f6a1782512016559",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f4c0bfce1a03aa6f17c3",
+    "url": "/portfolio/static/css/main.bbffe0fb.chunk.css"
+  },
+  {
+    "revision": "8ff711a1f9d51ddc2904",
+    "url": "/portfolio/static/js/2.0fd12f85.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.0fd12f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c0bfce1a03aa6f17c3",
+    "url": "/portfolio/static/js/main.15cffc98.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "8063064f5c4484f9a086ffaf84a9fc68",
+    "url": "/portfolio/static/media/logo2b.8063064f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2883bbe27741bad177e9859af40df1cd.js"
+  "/portfolio/precache-manifest.290664f98522a001de1a105f6e3ca42d.js"
 );
 
 self.addEventListener('message', (event) => {
